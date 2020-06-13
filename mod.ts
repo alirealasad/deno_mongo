68a5ecaa4d1d070380bd566c35b65780fc263b4d
ASAD <alirealasad@gmail.com>
2020-06-13 20:13:08 +0530

    try

--- mod.ts
+++ mod.ts
@@ -6,7 +6,7 @@ export * from "./ts/database.ts";
 export * from "./ts/result.ts";
 export { ObjectId } from "./ts/types.ts";
 export * from "./ts/util.ts";
-export const VERSION = "v0.8.0";
+export const VERSION = "v0.8.1";
 export const RELEASE_URL =
   `https://github.com/alirealasad/deno_mongo/releases/download/${VERSION}`;
 
